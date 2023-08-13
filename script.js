57db89cae54eb005552dde8534e7310a4d2e522e
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:27:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+// Complete the js code
+
+const limit = 50; // Heap memory usage limit in MB
+let elements = []; // Array to hold generated DOM elements
+
+// Function to generate DOM elements
+const generateElements = () => {
+  // complete the function
+  updateMemoryUsage();
+};
+
+// Function to remove DOM elements
+const removeElements = () => {
+  // complete the function
+  updateMemoryUsage();
+};
+
+// Function to update memory usage display
+const updateMemoryUsage = () => {
+  // Complete this function
+};
+
+// Add event listeners to buttons
+document.getElementById("generate").addEventListener("click", generateElements);
+document.getElementById("remove").addEventListener("click", removeElements);
+
+// Set interval to update memory usage every second
+setInterval(updateMemoryUsage, 1000);
